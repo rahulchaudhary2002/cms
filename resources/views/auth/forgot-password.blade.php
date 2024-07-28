b08c5045642f4aad5b7a740a74e0aa4f319629db
Rahul Chaudhary <rahulchaudharyrbc@gmail.com>
2024-07-28 22:40:19 +0545

    fix: fix password reset link alert

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -7,9 +7,13 @@
         <div class="auth__form-section">
             <img src="{{ asset('assets/images/cms.png') }}" alt="CMS" class="logo">
             <div class="form-content">
+                @if (Session::has('status'))
+                <div class="alert alert-success" role="alert">
+                    Password reset link has been send to your email!
+                </div>
+                @endif
                 <h6>Reset Password</h6>
                 <p>Enter your email and we will send you a password reset link on your inbox</p>
-
                 <form method="POST" action="{{ route('password.email') }}" class="auth-form">
                     @csrf
                     <div class="input-group">
